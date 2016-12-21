5475fb8359f8ae170f2ce17f3f191c737cbe7ee4
Maxim Nazarenko <maks.nazarenko@gmail.com>
2016-12-21 16:05:55 +0200

    fix tests

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
 
 SLAPD_DOMAIN_NAME="$(echo $SLAPD_DOMAIN| sed -e 's/^\([^\.]*\)\..*$/\1/g')"
 SLAPD_DOMAIN_DC_FORMAT="dc=$(echo $SLAPD_DOMAIN| sed -e 's/\./,dc=/g')"
-
+SLAPD_PASSWORD_HASH=$(slappasswd -n -s "$SLAPD_PASSWORD")
 
 TEST_USER="user_$(date +'%s')"
 TEST_USER_PASSWORD="test_pass"
